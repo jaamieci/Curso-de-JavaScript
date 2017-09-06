469e66110be0a72905682c2fa3e5213423f3529e
jaamieci <31671382+jaamieci@users.noreply.github.com>
2017-09-06 11:40:54 -0300

    Capítulo 7
    Asignación de valores a variables y uso de ellas.

---
+++ C07/script/main.js
@@ -0,0 +1,11 @@
+//var myFavouriteMovie = "Troll 2";
+var myFavouriteMovie = prompt("What is your favourite movie?");;
+alert("My favourite movie is: "+myFavouriteMovie);
+console.log(myFavouriteMovie);
+
+var randomNumber = Math.random(); // Genera un número random
+alert(randomNumber);
+
+var a = 2;
+var b = 3;
+document.write(a+b);
