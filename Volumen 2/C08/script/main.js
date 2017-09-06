b711f6cd82a2f96f404ab011b0743fadfbb21b0f
jaamieci <31671382+jaamieci@users.noreply.github.com>
2017-09-06 14:50:32 -0300

    Capítulo 8
    Uso de arreglos, se insertan nuevos elementos.

---
+++ Volumen 2/C08/script/main.js
@@ -0,0 +1,5 @@
+var friends = ["Mark", "Lisa", "Denny"];
+//console.log(friends[2]);
+friends[3] = "Bob";
+
+friends.push("Marry"); //Si no nos sabemos el índice que sigue despues del último elemento
