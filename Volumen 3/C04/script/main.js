6362bcb8d3d30b920ff513fbd44be52bf2c3b890
jaamieci <31671382+jaamieci@users.noreply.github.com>
2017-09-06 16:59:14 -0300

    Capítulo 4
    Creación de objetos a través de un constructor.

---
+++ Volumen 3/C04/script/main.js
@@ -0,0 +1,18 @@
+var mark = {
+    name: "Mark",
+    tshirColor: "navy blue"
+};
+
+var lisa = {
+    name: "Lisa",
+    tshirColor: "red"
+};
+
+//Constructor
+function Friend(name, tshirColor) {
+    this.name = name;
+    this.tshirColor = tshirColor;
+}
+
+var denny = new Friend("Denny", "green");
+
