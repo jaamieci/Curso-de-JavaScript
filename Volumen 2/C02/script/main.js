b405001c949d8bc050907456f02c1ca931c52453
jaamieci <31671382+jaamieci@users.noreply.github.com>
2017-09-06 13:08:39 -0300

    Capítulo 2 Volumen 2 del curso
    Uso de la sentencia switch.

---
+++ Volumen 2/C02/script/main.js
@@ -0,0 +1,27 @@
+//Utilizando la sentencia switch
+
+var sign = prompt("what is your astrological sign?");
+
+switch(sign){
+	case "Taurus":
+		alert("You are Taurus");
+		break;
+	case "Virgo":
+		alert("You are Virgo");
+		break;
+	case "Leo":
+		alert("You are Leo");
+		break;
+	case "Aries":
+		alert("You are Aries");
+		break;
+	case "Scorpion":
+		alert("You are scorpion");
+		break;
+	case "Libra":
+		alert("You are Libra");
+		break;
+	default:
+		alert("I dont know that astrological sign");
+		break;
+}
