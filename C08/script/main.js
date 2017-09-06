053ff558321b3a2557d1cd668ea20ebf2a80e9e1
jaamieci <31671382+jaamieci@users.noreply.github.com>
2017-09-06 11:52:21 -0300

    Capítulo 8
    Uso de la sentencia if/else.

---
+++ C08/script/main.js
@@ -0,0 +1,7 @@
+var answer = prompt("What is the capital of New York?");
+ //Uso de la sentencia if/else
+if(answer === "Albany"){
+	alert("You are correct");
+}else{
+	alert("Sorry"+" "+answer+" "+"is not correct");
+}
