6c1fc0ef25632e4cbe686e9e6475e817a0edbd5b
jaamieci <31671382+jaamieci@users.noreply.github.com>
2017-09-06 14:42:20 -0300

    Capítulo 7
    Se continúa con funciones, pero esta vez realizando pequeños cálculos.

---
+++ Volumen 2/C07/script/main.js
@@ -0,0 +1,18 @@
+//Calcula el área de un cuadrado
+function calculateAreaSquare(side){
+	console.log(side * side);
+}
+
+calculateAreaSquare(5);
+
+function calculateAreaRectangle(width, height){
+	console.log(width * height);
+}
+
+calculateAreaRectangle(10, 5);
+
+function randomNumber(){
+	console.log(Math.random());
+}
+
+randomNumber();
