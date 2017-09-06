aaddff757a10c1e3f8418e920c28680c63395430
jaamieci <31671382+jaamieci@users.noreply.github.com>
2017-09-06 14:30:00 -0300

    Capítulo 6
    Uso de funciones.

---
+++ Volumen 2/C06/script/main.js
@@ -0,0 +1,15 @@
+/*var name = "Mark";
+console.log("Oh hi " + name);
+
+var name2 = "Lisa";
+console.log("Oh hi " + name2);
+
+alert("Some text!");*/
+
+function sayHello(name){
+	console.log("Oh hi " + name);
+}
+
+sayHello("Mark");
+sayHello("Lisa");
+sayHello("Denny");
