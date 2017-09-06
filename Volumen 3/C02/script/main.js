c320e875d84327df8596a0ef591fed76b878ea7d
jaamieci <31671382+jaamieci@users.noreply.github.com>
2017-09-06 16:21:27 -0300

    Capítulo 2 Volumen 3 del curso
    Creación de un objeto para la representación de sus características.

---
+++ Volumen 3/C02/script/main.js
@@ -0,0 +1,17 @@
+//Descripción de un café
+var coffeeFlavor = "espresso";
+var coffeeTemperature = "piping host";
+var coffeeOunces = 100;
+var coffeMilk = true;
+
+//Descripción de un café a través de la creación de un objeto
+
+var myCoffee = {
+	flavor: "espresso", 
+	temperature: "piping host",
+	ounces: 100,
+	milk: true
+};
+
+//myCoffee.flavor;
+alert(myCoffee.ounces);
