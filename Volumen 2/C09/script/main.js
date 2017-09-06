e0beffbd90830806e52f35ba25ac83b51084ad54
jaamieci <31671382+jaamieci@users.noreply.github.com>
2017-09-06 15:01:44 -0300

    Capítulo 9
    Uso de ciclo For para el manejo de arreglos.

---
+++ Volumen 2/C09/script/main.js
@@ -0,0 +1,9 @@
+var friends = ["Mark", "Lisa", "Denny", "Bob", "Marry"];
+
+function greetFriends(){
+	for(var i = 0; i < friends.length; i++){
+		console.log("Oh hi " + friends[i]);
+	}
+}
+
+greetFriends();
