4f07f97fdeaaf5b6f09c094072ff4f48290fa9ad
jaamieci <31671382+jaamieci@users.noreply.github.com>
2017-09-06 10:49:56 -0300

    Capítulo 4
    Entendiendo el uso de la consola usando las instrucciones 
    console.log()
    console.warn()
    console.error()

---
+++ C04/script/main.js
@@ -0,0 +1,7 @@
+2*2
+/*
+4
+*/
+console.log("Hello World");
+console.warn("Warning!!!");  //Esto es una alerta de warning en la consola
+console.error("Error!!!");   //Mensaje de error en la consola
