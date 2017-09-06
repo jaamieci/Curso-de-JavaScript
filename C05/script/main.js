d56af973b139463b3fae957df279adbd66599809
jaamieci <31671382+jaamieci@users.noreply.github.com>
2017-09-06 11:13:12 -0300

    Capítulo 5
    Tipos de datos en JavaScript.

---
+++ C05/script/main.js
@@ -0,0 +1,48 @@
+//String, number and boolean values
+
+console.log("Una cadena");
+
+2+2
+/*
+4
+*/
+
+//Concatenación
+2+"2"
+/*
+22
+*/
+document.write(2+"2");
+document.write("<br>");
+document.write(typeof(2+2)); //Tipo de dato number
+document.write("<br>");
+document.write(10 < 2); //False
+document.write("<br>");
+document.write(typeof(10 < 2)); //Tipo de dato boolean
+
+"Hello" + "World"
+/*
+HelloWorld
+*/
+
+"Hello" + " " +"World"
+/*
+Hello World
+*/
+
+10 > 2
+/*
+true
+*/
+
+10 < 2
+/*
+false
+*/
+
+typeof(10 < 2);
+/*
+boolean
+*/
+
+//Null (valor que no existe)
