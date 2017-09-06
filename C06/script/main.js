6d93b3d3a73b46386eb20a9cabbe73e2c309aa8d
jaamieci <31671382+jaamieci@users.noreply.github.com>
2017-09-06 11:28:31 -0300

    Capítulo 6
    Uso de variables e identificación de las partes de una variable.

---
+++ C06/script/main.js
@@ -0,0 +1,25 @@
+
+"Hello"+" World"
+/*
+"Hello World"
+*/
+
+typeof(3+"6"); //String
+typeof(c); //Undifined
+
+//var (palabra clave que indica que una variable es creada)
+var a = 2;
+//a (nombre de la variable)
+//2 (valor de la variable)
+
+/*Las variables no pueden comenzar con un número,
+no se permiten espacios, no permite palabras reservadas
+y no puede usar ningún signo excepto _ or $*/
+typeof(a); //number
+
+var b = 4;
+
+typeof(a+b); //number
+
+var myName = "Johny";
+alert(myName);
